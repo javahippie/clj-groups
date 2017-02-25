b3e60ee3a669c5f786bb2c2b887928d84064e9d3
Tim ZÖller <mail@tim-zoeller.de>
2017-02-25 15:37:23 +0100

    Restructuring

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject clj-groups "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
