80569deb3fe544bf8b3b692376109edb1e5d8a16
Tim ZÖller <mail@tim-zoeller.de>
2017-02-26 18:15:03 +0100

    Further toying around

--- src/java/de/cljgroups/ReceiverAdapterWrapper.java
+++ src/java/de/cljgroups/ReceiverAdapterWrapper.java
@@ -2,7 +2,17 @@ package de.cljgroups;
 
 import org.jgroups.JChannel;
 import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+import org.jgroups.Message;
 
 public class ReceiverAdapterWrapper extends ReceiverAdapter {
     
+    public void viewAccepted(View view) {
+        System.out.println("** view: " + view);
+    }
+
+    public void receive(Message msg) {
+        System.out.println(msg.getObject().toString());
+    }
+
 }
