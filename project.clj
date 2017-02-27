58e4e240b2fa71161086f33fa2bac81e5b3b1316
Tim Zöller <mail@tim-zoeller.de>
2017-02-27 11:16:19 +0100

    Removed Java Code, we don't need it

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject clj-groups "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :min-lein-version "2.7.1"
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
-  :javac-options ["-target" "1.8" "-source" "1.8"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- src/clj_groups/channel.clj
+++ src/clj_groups/channel.clj
@@ -1,18 +1,41 @@
-(ns clj-groups.channel)
+(ns clj-groups.channel
+  (:gen-class))
 
 (def channel (atom {}))
 
+(defn reify-test []
+  (reify org.jgroups.Receiver
+    (^void receive [this 
+                    ^org.jgroups.Message msg]
+      (println msg))
+    (getState [this
+                     output]
+      (println output))
+    (setState [this
+               input]
+      (println input))
+    (viewAccepted [this
+                   view]
+      (println view))
+    (suspect [this
+              mbr]
+      (println mbr))
+    (block [this]
+      (println "block"))
+    (unblock [this]
+      (println "unblock"))))
+
 (defn- connect!
   "Joins to the specified cluster"
-  [channel identifier]
-  (.setReceiver channel (de.cljgroups.ReceiverAdapterWrapper.))
+  [channel identifier callback]
+  (.setReceiver channel (reify-test))
   (.connect channel identifier))
 
 (defn setup! 
   "Creates an instance of a channel and connects it via the given identifier. Uses default protocol settings" 
-  [identifier]
+  [identifier callback]
   (reset! channel (org.jgroups.JChannel.))
-  (connect! @channel identifier))
+  (connect! @channel identifier callback))
 
 (defn setup-with-settings!
   "Creates an instance of a channel and connects it via the given identifier. Uses the passed settings"
@@ -32,3 +55,4 @@
   []
   (.close @channel)
   (reset! channel {}))
+
